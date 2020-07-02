caed132fa1a789be71fdd2b9e08f33ee2f30ba93
samolevich <samolevich@gmail.com>
2020-07-02 15:25:18 +0300

    add navbar function component and some styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
+import { Navbar } from './components/Navbar';
 
-const App: React.FunctionComponent = () => <>x</>
+const App: React.FunctionComponent = () => <Navbar />
 
 export default App;
